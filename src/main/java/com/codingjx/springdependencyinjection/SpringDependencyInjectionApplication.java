444189afaad1f33010af2b9e345acec1b65d2d74
chiahsingchu <chiahsingtony@gmail.com>
2023-08-06 21:05:57 -0700

    Add spring context example

--- src/main/java/com/codingjx/springdependencyinjection/SpringDependencyInjectionApplication.java
+++ src/main/java/com/codingjx/springdependencyinjection/SpringDependencyInjectionApplication.java
@@ -1,13 +1,21 @@
 package com.codingjx.springdependencyinjection;
 
+import com.codingjx.springdependencyinjection.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDependencyInjectionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        String greeting = myController.sayHello();
+        System.out.println(greeting);
     }
 
 }
