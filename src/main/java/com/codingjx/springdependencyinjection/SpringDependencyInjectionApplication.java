628c58e20d1bb7b591d002db97f88b26ffd6eba1
chiahsingchu <chiahsingtony@gmail.com>
2023-08-16 21:18:27 -0700

    Dependency injection using spring

--- src/main/java/com/codingjx/springdependencyinjection/SpringDependencyInjectionApplication.java
+++ src/main/java/com/codingjx/springdependencyinjection/SpringDependencyInjectionApplication.java
@@ -1,6 +1,9 @@
 package com.codingjx.springdependencyinjection;
 
+import com.codingjx.springdependencyinjection.controller.ConstructorInjectedController;
 import com.codingjx.springdependencyinjection.controller.MyController;
+import com.codingjx.springdependencyinjection.controller.PropertyInjectedController;
+import com.codingjx.springdependencyinjection.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,18 @@ public class SpringDependencyInjectionApplication {
 
         String greeting = myController.sayHello();
         System.out.println(greeting);
+
+        System.out.println("------------ Property");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("------------ Setter");
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+        System.out.println(setterInjectedController.getGreeting());
+
+        System.out.println("------------ Constructor");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+        System.out.println(constructorInjectedController.getGreeting());
     }
 
 }
