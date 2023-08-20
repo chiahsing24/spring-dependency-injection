e3a18fce90409e3d4d1fdd833cc7ddb64e678836
chiahsingchu <chiahsingtony@gmail.com>
2023-08-20 14:40:31 -0700

    Add example of how to use Profile, Bean with Qualifier annotations in Java Bean Configuration

--- src/main/java/com/codingjx/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/com/codingjx/springdependencyinjection/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package com.codingjx.springdependencyinjection.config;
 
-import com.codingjx.springdependencyinjection.service.ConstructorGreetingService;
-import com.codingjx.springdependencyinjection.service.PropertyInjectedGreetingService;
-import com.codingjx.springdependencyinjection.service.SetterInjectedGreetingService;
+import com.codingjx.springdependencyinjection.service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService I18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
