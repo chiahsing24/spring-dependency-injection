1e5d555eb144ba180a26a8fff63dd7314de04f1f
chiahsingchu <chiahsingtony@gmail.com>
2023-08-20 20:15:31 -0700

    Example of using properties source

--- src/main/java/com/codingjx/springdependencyinjection/SpringDependencyInjectionApplication.java
+++ src/main/java/com/codingjx/springdependencyinjection/SpringDependencyInjectionApplication.java
@@ -1,6 +1,7 @@
 package com.codingjx.springdependencyinjection;
 
 import com.codingjx.springdependencyinjection.controller.*;
+import com.codingjx.springdependencyinjection.datasource.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,29 +18,41 @@ public class SpringDependencyInjectionApplication {
         PetController petController = ctx.getBean("petController", PetController.class);
         System.out.println("--- The Best Pet is ---");
         System.out.println(petController.whichPetIsTheBest());
+        System.out.println();
 
         I18nController i18nController = (I18nController) ctx.getBean("i18nController");
         System.out.println(i18nController.sayHello());
+        System.out.println();
 
         MyController myController = (MyController) ctx.getBean("myController");
 
         System.out.println("------------ Primary Bean");
         System.out.println(myController.sayHello());
+        System.out.println();
 
 //        String greeting = myController.sayHello();
 //        System.out.println(greeting);
+//        System.out.println();
 
         System.out.println("------------ Property");
         PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
         System.out.println(propertyInjectedController.getGreeting());
+        System.out.println();
 
         System.out.println("------------ Setter");
         SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
         System.out.println(setterInjectedController.getGreeting());
+        System.out.println();
 
         System.out.println("------------ Constructor");
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
         System.out.println(constructorInjectedController.getGreeting());
+        System.out.println();
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getJdbcUrl());
     }
 
 }
--- src/main/java/com/codingjx/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/com/codingjx/springdependencyinjection/config/GreetingServiceConfig.java
@@ -2,14 +2,28 @@ package com.codingjx.springdependencyinjection.config;
 
 import com.codingjx.pets.PetService;
 import com.codingjx.pets.PetServiceFactory;
+import com.codingjx.springdependencyinjection.datasource.FakeDataSource;
 import com.codingjx.springdependencyinjection.repository.EnglishGreetingRepository;
 import com.codingjx.springdependencyinjection.repository.EnglishGreetingRepositoryImpl;
 import com.codingjx.springdependencyinjection.service.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:di-config.xml")
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${codingjx.username}") String username,
+                                  @Value("${codingjx.password}") String password,
+                                  @Value("${codingjx.jdbcUrl}")String jdbcUrl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcUrl(jdbcUrl);
+        return fakeDataSource;
+    }
 
     @Bean
     PetServiceFactory petServiceFactory() {
