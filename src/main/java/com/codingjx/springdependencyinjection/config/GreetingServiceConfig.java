7ed7b8b08ac31d23e53749273d58523cee70d712
chiahsingchu <chiahsingtony@gmail.com>
2023-08-20 20:25:32 -0700

    Remove datasource.properties and use spring boot's application.properties directly

--- src/main/java/com/codingjx/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/com/codingjx/springdependencyinjection/config/GreetingServiceConfig.java
@@ -9,7 +9,7 @@ import com.codingjx.springdependencyinjection.service.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+//@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:di-config.xml")
 @Configuration
 public class GreetingServiceConfig {
